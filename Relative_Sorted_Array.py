94907702091d8b84fe68f9c3a1932e5886bbcbe5
Mrunmayee More <66308881+mrumayee@users.noreply.github.com>
2022-03-27 11:00:10 +0530

    Update Relative_Sorted_Array.py

--- Relative_Sorted_Array.py
+++ Relative_Sorted_Array.py
@@ -1,3 +1,11 @@
+/*
+
+Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+
+*/
+
 class Solution(object):
     def relativeSortArray(self, arr1, arr2):
         res = []
